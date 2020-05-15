1af86423d84933aeb5a6723240c2b80f0e41f7a6
Denis Bystruev <dbystruev@me.com>
2020-05-15 16:54:29 +0300

    Fixed randomize.sh

--- api/randomize.sh
+++ api/randomize.sh
@@ -22,7 +22,7 @@ check_file_exists "$FILE2"
 # Check that the file does not exist
 function check_file_absent {
     if [ -f "$1" ]; then
-        echi "File $1 exists. Remove it to continue"
+        echo "File $1 exists. Remove it to continue"
         exit
     fi
 }
@@ -36,20 +36,30 @@ check_file_absent "$FILE2.old"
 # Replace getResponseToken() keys with random values
 function replace_keys {
     cat "$1" \
-    | sed s/0x11111111/$RANDOM/ \
-    | sed s/0x22222222/$RANDOM/ \
-    | sed s/0x33333333/$RANDOM/ \
-    | sed s/0x44444444/$RANDOM/ \
-    | sed s/0x55555555/$RANDOM/ \
-    | sed s/0x66666666/$RANDOM/ \
-    | sed s/0x77777777/$RANDOM/ \
-    | sed s/0x88888888/$RANDOM/ \
+    | sed s/0x11111111/$2/ \
+    | sed s/0x22222222/$3/ \
+    | sed s/0x33333333/$4/ \
+    | sed s/0x44444444/$5/ \
+    | sed s/0x55555555/$6/ \
+    | sed s/0x66666666/$7/ \
+    | sed s/0x77777777/$8/ \
+    | sed s/0x88888888/$9/ \
     > "$1.new"
 }
 
+# Generate random values
+RANDOM1=$RANDOM
+RANDOM2=$RANDOM
+RANDOM3=$RANDOM
+RANDOM4=$RANDOM
+RANDOM5=$RANDOM
+RANDOM6=$RANDOM
+RANDOM7=$RANDOM
+RANDOM8=$RANDOM
+
 # Replace getResponseToken() keys with random values
-replace_keys "$FILE1"
-replace_keys "$FILE2"
+replace_keys "$FILE1" $RANDOM1 $RANDOM2 $RANDOM3 $RANDOM4 $RANDOM5 $RANDOM6 $RANDOM7 $RANDOM8
+replace_keys "$FILE2" $RANDOM1 $RANDOM2 $RANDOM3 $RANDOM4 $RANDOM5 $RANDOM6 $RANDOM7 $RANDOM8
 
 # Move current files to old and new to current
 mv "$FILE1" "$FILE1.old"
